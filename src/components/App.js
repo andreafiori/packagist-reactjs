3f099f734af7e7320a5a4f5502db41ded207cf8e
Andrea Fiori <info@andreafiori.net>
2018-11-15 13:03:57 +0100

    App test added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import PackageList from './PackageList.js';
-import LoadingSpinner from './LoadingSpinner.js';
 import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
     this.state = {
       error: null,
       pack: '',
@@ -29,11 +29,9 @@ class App extends Component {
     this.setState({ loading: true, error: null, items: null }, () => {
       axios.get('https://packagist.org/search.json', { params: { q: this.state.pack } })
         .then(function (response) {
-          // console.log(response.data);
           self.setState({loading: false, error: null, items: response.data});
         })
         .catch(function (error) {
-          // console.log(error);
           self.setState({loading: false, items: null, error: error});
         });
     });
@@ -49,32 +47,28 @@ class App extends Component {
   }
 
   render() {
-    const { loading, error, items } = this.state;
+    const { error, items } = this.state;
     return (
       <Container>
         <h1>Search PHP package using the Packagist APIs</h1>
-        <p>Packagist is the main Composer repository. It aggregates public PHP packages installable with Composer.</p>
+        <p>Packagist is the main <a href="https://getcomposer.org/" target="_blank" rel="noopener noreferrer" title="Composer official website">Composer</a> repository. It aggregates public PHP packages installable with Composer.</p>
         <form className="form-inline" onSubmit={this.handleSubmit}>
           <div className="form-group mr-2 mb-2">
             <label htmlFor="pack" className="sr-only">Package</label>
             <input className="form-control" required="required" id="pack" type="text" placeholder="Package..." value={this.state.pack} onChange={this.handleChange} title="Type the package to search" />
           </div>
-          <button type="submit" className="btn btn-primary" title="Start a research">Search</button>
+          <button type="submit" id="btn-search" className="btn btn-primary" title="Start a research">Search</button>
           <button type="button" className="btn btn-danger ml-2" title="Reset form and result" disabled={this.canDisableButton()} onClick={this.onReset.bind(this)}>Reset</button>
         </form>
 
-        {error != null &&
+        { error != null &&
           <div className="alert alert-danger">
             <h3>Error calling the API</h3>
             <p>The API from packagist.org did not answered correctly. Please report this error.</p>
           </div>
         }
 
-        { loading &&
-          <LoadingSpinner />
-        }
-
-        {items != null &&
+        { items != null &&
           <div><PackageList packages={items} /></div>
         }
       </Container>
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import mockAxios from "axios";
+// import unsplash from "../unsplash";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('renders without crashing', () => {
+  const app = shallow(<App />);
+  const state = {
+    error: null,
+    pack: '',
+    items: null,
+    loading: false,
+  };
+
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+  
+  it('initialize default state', () => {
+    expect(app.state()).toEqual(state);
+  });
+
+  it('renders simulate typing a package', () => {
+    app.find('#pack').simulate('change', { target: { value: 'Zend' } });
+    app.find('#btn-search').simulate('click');
+  });
+
+  it('renders without crashing using ReactDOM', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
 });
